a4cb492d14d2e65840818ed4fe169011b30be23a
Kevin Robert Stravers <macocio@gmail.com>
2018-08-18 11:06:54 +0200

    Set the correct scribble document

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "logger")
 (define deps '("base"))
 (define build-deps '("sandbox-lib" "scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/rkt-logger.scrbl" ())))
+(define scribblings '(("scribblings/logger.scrbl" ())))
 (define pkg-desc "Simple effective logging in Racket")
 (define version "0.1.0")
 (define pkg-authors '("Kevin Robert Stravers"))
